0422419d45df8c59f833e84f0e14cbb3c0e2fec2
Noureldien <noureldien@live.com>
2014-10-28 00:42:05 +0000

    Commit

--- HybridImage/MainWindow.xaml.cs
+++ HybridImage/MainWindow.xaml.cs
@@ -33,34 +33,33 @@ namespace HybridImage
         /// </summary>
         private string[][] imagePairs = new string[][]
         {
+            new string[] {"einstein.bmp", "marilyn.bmp" },
             new string[] {"bicycle.bmp", "motorcycle.bmp" },
             new string[] {"cat.bmp", "dog.bmp" },
-            new string[] {"bird.bmp", "fish.bmp" },
-            new string[] {"einstein.bmp", "marilyn.bmp" },
+            new string[] {"bird.bmp", "fish.bmp" },            
             new string[] {"plane.bmp", "submarine.bmp" },
         };
 
         /// <summary>
         /// Low-pass filter (smooth image that will appear from a far distance).
         /// </summary>
-        private IplImage image1;
+        private System.Drawing.Bitmap bitmap1;
         /// <summary>
         /// High-pass filter (sharp image that will appear from a near distance).
         /// </summary>
-        private IplImage image2;
+        private System.Drawing.Bitmap bitmap2;
         /// <summary>
         /// Hybrid image.
         /// </summary>
-        private IplImage hybridImage;
-        /// <summary>
-        /// Which image-pair is selected.
-        /// </summary>
-        private int selectedPairsIndex;
-
+        private System.Drawing.Bitmap hybridImage;
         /// <summary>
         /// Guassian matrix/template used to apply template convolution to the images (Guassian smoothing).
         /// </summary>
         private double[,] convolutionTemplate;
+        /// <summary>
+        /// Which image-pair is selected.
+        /// </summary>
+        private int selectedPairsIndex;
 
         #endregion
 
@@ -78,6 +77,11 @@ namespace HybridImage
         private void WindowMain_Loaded(object sender, RoutedEventArgs e)
         {
             Initialize();
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            Convolution();
         }
 
         #endregion
@@ -103,11 +107,8 @@ namespace HybridImage
         {
             string source1 = String.Format(@"Images/{0}", imagePairs[selectedPairsIndex][0]);
             string source2 = String.Format(@"Images/{0}", imagePairs[selectedPairsIndex][1]);
-            //source1 = System.IO.Path.Combine(folder, source1);
-            //source1 = System.IO.Path.Combine(folder, source2);
-
-            image1 = new IplImage(source1);
-            image2 = new IplImage(source2);
+            bitmap1 = new System.Drawing.Bitmap(source1);
+            bitmap2 = new System.Drawing.Bitmap(source2);
         }
 
         private void Convolution()
@@ -118,6 +119,44 @@ namespace HybridImage
             // I2: image 2
             // G1: gaussian low-pass filter
             // 1-G2: gaussian high-pass filter
+
+            int templateWidth = convolutionTemplate.GetLength(1);
+            int templateHeight = convolutionTemplate.GetLength(0);
+            int xOffset = templateWidth / 2;
+            int yOffset = templateHeight / 2;
+
+            System.Drawing.Bitmap i1 = new System.Drawing.Bitmap(bitmap1.Width, bitmap1.Height, bitmap1.PixelFormat);
+            System.Drawing.Bitmap i2 = new System.Drawing.Bitmap(bitmap2.Width, bitmap2.Height, bitmap2.PixelFormat);
+
+            int r, g, b;
+            System.Drawing.Color pixel;
+
+            for (int imageY = yOffset; imageY < i1.Height - yOffset; imageY++)
+            {
+                for (int imageX = xOffset; imageX < i1.Width - xOffset; imageX++)
+                {
+                    r = 0;
+                    g = 0;
+                    b = 0;
+
+                    for (int templateY = 0; templateY < templateHeight; templateY++)
+                    {
+                        for (int templateX = 0; templateX < templateWidth; templateX++)
+                        {
+                            pixel = bitmap1.GetPixel(imageX + templateX - xOffset, imageY + templateY - yOffset);
+                            r += (int)(pixel.R * convolutionTemplate[templateY, templateX]);
+                            g += (int)(pixel.G * convolutionTemplate[templateY, templateX]);
+                            b += (int)(pixel.B * convolutionTemplate[templateY, templateX]);
+                        }
+                    }
+
+                    pixel = System.Drawing.Color.FromArgb(r, g, b);
+                    i1.SetPixel(imageX, imageY, pixel);
+                }
+            }
+
+            imageView3.Source = BitmapToBitmapSource(i1);
+
         }
 
         private void TryOne()
@@ -154,6 +193,28 @@ namespace HybridImage
                     Cv.WaitKey();
                 }
             }
+        }
+
+        private BitmapSource BitmapToBitmapSource(System.Drawing.Bitmap source)
+        {
+            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                          source.GetHbitmap(),
+                          IntPtr.Zero,
+                          Int32Rect.Empty,
+                          BitmapSizeOptions.FromEmptyOptions());
+        }
+
+        private System.Drawing.Bitmap BitmapSourceToBitmap(BitmapSource bitmapsource)
+        {
+            System.Drawing.Bitmap bitmap;
+            using (var outStream = new System.IO.MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new System.Drawing.Bitmap(outStream);
+            }
+            return bitmap;
         }
 
         #endregion
