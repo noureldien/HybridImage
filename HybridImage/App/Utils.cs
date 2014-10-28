c78a7295d4859166f751e019dc0ea406aef709de
Noureldien <noureldien@live.com>
2014-10-28 19:55:48 +0000

    Commit

---
+++ HybridImage/App/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HybridImage
+{
+    public class Utils
+    {
+        /// <summary>
+        /// Write the sent object to the debug.
+        /// </summary>
+        /// <param name="obj"></param>
+        public static void DebugLine(object obj)
+        {
+            System.Diagnostics.Debug.WriteLine(obj);
+        }
+    }
+}
--- HybridImage/MainWindow.xaml.cs
+++ HybridImage/MainWindow.xaml.cs
@@ -1,9 +1,9 @@
-﻿using OpenCvSharp;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,10 +22,6 @@ namespace HybridImage
     /// </summary>
     public partial class MainWindow : Window
     {
-        #region Constants
-
-        #endregion
-
         #region Variables
 
         /// <summary>
@@ -33,33 +29,37 @@ namespace HybridImage
         /// </summary>
         private string[][] imagePairs = new string[][]
         {
-            new string[] {"einstein.bmp", "marilyn.bmp" },
-            new string[] {"bicycle.bmp", "motorcycle.bmp" },
-            new string[] {"cat.bmp", "dog.bmp" },
-            new string[] {"bird.bmp", "fish.bmp" },            
-            new string[] {"plane.bmp", "submarine.bmp" },
+            new string[] {"marilyn.bmp", "einstein.bmp" },
+            new string[] {"motorcycle.bmp", "bicycle.bmp" },
+            new string[] {"dog.bmp", "cat.bmp" },
+            new string[] {"bird.bmp", "plane.bmp" },
+            new string[] {"fish.bmp", "submarine.bmp" },
         };
 
         /// <summary>
         /// Low-pass filter (smooth image that will appear from a far distance).
         /// </summary>
-        private System.Drawing.Bitmap bitmap1;
+        private byte[, ,] image1;
         /// <summary>
         /// High-pass filter (sharp image that will appear from a near distance).
         /// </summary>
-        private System.Drawing.Bitmap bitmap2;
-        /// <summary>
-        /// Hybrid image.
-        /// </summary>
-        private System.Drawing.Bitmap hybridImage;
-        /// <summary>
-        /// Guassian matrix/template used to apply template convolution to the images (Guassian smoothing).
-        /// </summary>
-        private double[,] convolutionTemplate;
+        private byte[, ,] image2;
         /// <summary>
         /// Which image-pair is selected.
         /// </summary>
         private int selectedPairsIndex;
+        /// <summary>
+        /// Thread to do template convolution under it to release pressure from UI thread.
+        /// </summary>
+        private Thread thread;
+        private System.Drawing.Bitmap bitmap1;
+        private System.Drawing.Bitmap bitmap2;
+        private int lpfIterations;
+        private int hpfIterations;
+        private int lpfDimension;
+        private int hpfDimension;
+        private decimal hpfBrightness;
+        private int hpfLuminosity;
 
         #endregion
 
@@ -79,9 +79,41 @@ namespace HybridImage
             Initialize();
         }
 
+        private void WindowMain_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisposeObjects();
+        }
+
         private void ButtonStart_Click(object sender, RoutedEventArgs e)
         {
-            Convolution();
+            buttonStart.IsEnabled = false;
+            buttonStart.Content = "Working!";
+            progressBar.Visibility = System.Windows.Visibility.Visible;
+            progressBar.Value = 0;
+
+            imageView3.Source = null;
+            imageView4.Source = null;
+            imageView5.Source = null;
+
+            lpfIterations = (int)byteUpDownLpfIterations.Value;
+            hpfIterations = (int)byteUpDownHpfIterations.Value;
+            lpfDimension = (int)byteUpDownLpfDimension.Value;
+            hpfDimension = (int)byteUpDownHpfDimension.Value;
+            hpfBrightness = (decimal)decimalUpDownHpfBrightness.Value;
+            hpfLuminosity = (int)byteUpDownHpfLuminosity.Value;
+
+            // search thread
+            ThreadStart threadStart = new ThreadStart(Convolution);
+            thread = new Thread(threadStart);
+            thread.Start();
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            string tag = ((RadioButton)sender).Tag.ToString();
+            int choise = int.Parse(tag);
+            selectedPairsIndex = choise - 1;
+            LoadImage();
         }
 
         #endregion
@@ -90,16 +122,6 @@ namespace HybridImage
 
         private void Initialize()
         {
-            convolutionTemplate = new double[,]
-            {
-                { 0.002, 0.013, 0.022, 0.013, 0.002 },
-                { 0.013, 0.060, 0.098, 0.060, 0.013 },
-                { 0.022, 0.098, 0.162, 0.098, 0.022 },
-                { 0.013, 0.060, 0.098, 0.060, 0.013 },
-                { 0.002, 0.013, 0.022, 0.013, 0.002 },
-            };
-
-            selectedPairsIndex = 0;
             LoadImage();
         }
 
@@ -109,6 +131,8 @@ namespace HybridImage
             string source2 = String.Format(@"Images/{0}", imagePairs[selectedPairsIndex][1]);
             bitmap1 = new System.Drawing.Bitmap(source1);
             bitmap2 = new System.Drawing.Bitmap(source2);
+            imageView1.Source = BitmapToBitmapSource(bitmap1);
+            imageView2.Source = BitmapToBitmapSource(bitmap2);
         }
 
         private void Convolution()
@@ -120,20 +144,99 @@ namespace HybridImage
             // G1: gaussian low-pass filter
             // 1-G2: gaussian high-pass filter
 
-            int templateWidth = convolutionTemplate.GetLength(1);
-            int templateHeight = convolutionTemplate.GetLength(0);
+            double progressValue = 0.0;
+            double progressStep = 100 / (double)(lpfIterations + hpfIterations + 3);
+            double[,] g1, g2;
+            //g1 = g2 = new double[,]
+            //{
+            //    { 0.002, 0.013, 0.022, 0.013, 0.002 },
+            //    { 0.013, 0.060, 0.098, 0.060, 0.013 },
+            //    { 0.022, 0.098, 0.162, 0.098, 0.022 },
+            //    { 0.013, 0.060, 0.098, 0.060, 0.013 },
+            //    { 0.002, 0.013, 0.022, 0.013, 0.002 },
+            //};
+            g1 = GaussianKernel(5, 20);
+            g2 = GaussianKernel(13, 10);
+
+            image1 = BitmapToByteArray(bitmap1);
+            image2 = BitmapToByteArray(bitmap2);
+            progressValue += progressStep;
+            UpdateProgressBar(progressValue);
+
+            byte[, ,] i1 = image1;
+            for (int i = 0; i < lpfIterations; i++)
+            {
+                i1 = GaussianFilter(i1, g1);
+                progressValue += progressStep;
+                UpdateProgressBar(progressValue);
+            }
+
+            byte[, ,] i2 = image2;
+            for (int i = 0; i < hpfIterations; i++)
+            {
+                i2 = GaussianFilter(i2, g2);
+                progressValue += progressStep;
+                UpdateProgressBar(progressValue);
+            }
+            i2 = Subtract(image2, i2);
+            i2 = Increment(i2, hpfLuminosity);
+            i2 = Brightness(i2, hpfBrightness);
+            progressValue += progressStep;
+            UpdateProgressBar(progressValue);
+
+            // adding h = i1 + i2
+            byte[, ,] h = Add(i1, i2);
+
+            Finish(i1, i2, h);
+        }
+
+        private double[,] GaussianKernel(int length, double weight)
+        {
+            double[,] kernel = new double[length, length];
+            double sumTotal = 0;
+
+            int kernelRadius = length / 2;
+            double distance = 0;
+
+            double calculatedEuler = 1.0 / (2.0 * Math.PI * Math.Pow(weight, 2));
+
+            for (int filterY = -kernelRadius; filterY <= kernelRadius; filterY++)
+            {
+                for (int filterX = -kernelRadius; filterX <= kernelRadius; filterX++)
+                {
+                    distance = ((filterX * filterX) + (filterY * filterY)) / (2 * (weight * weight));
+                    kernel[filterY + kernelRadius, filterX + kernelRadius] = calculatedEuler * Math.Exp(-distance);
+                    sumTotal += kernel[filterY + kernelRadius, filterX + kernelRadius];
+                }
+            }
+
+            for (int y = 0; y < length; y++)
+            {
+                for (int x = 0; x < length; x++)
+                {
+                    kernel[y, x] = kernel[y, x] * (1.0 / sumTotal);
+                }
+            }
+
+            return kernel;
+        }
+
+        private byte[, ,] GaussianFilter(byte[, ,] source, double[,] kernel)
+        {
+            int sourceHeight = source.GetLength(0);
+            int sourceWidth = source.GetLength(1);
+            byte[, ,] result = new byte[sourceHeight, sourceWidth, 3];
+            int templateWidth = kernel.GetLength(1);
+            int templateHeight = kernel.GetLength(0);
+            int templateLength = kernel.Length;
             int xOffset = templateWidth / 2;
             int yOffset = templateHeight / 2;
 
-            System.Drawing.Bitmap i1 = new System.Drawing.Bitmap(bitmap1.Width, bitmap1.Height, bitmap1.PixelFormat);
-            System.Drawing.Bitmap i2 = new System.Drawing.Bitmap(bitmap2.Width, bitmap2.Height, bitmap2.PixelFormat);
+            double r, g, b;
 
-            int r, g, b;
-            System.Drawing.Color pixel;
-
-            for (int imageY = yOffset; imageY < i1.Height - yOffset; imageY++)
+            for (int imageY = yOffset; imageY < sourceHeight - yOffset; imageY++)
             {
-                for (int imageX = xOffset; imageX < i1.Width - xOffset; imageX++)
+                for (int imageX = xOffset; imageX < sourceWidth - xOffset; imageX++)
                 {
                     r = 0;
                     g = 0;
@@ -143,65 +246,181 @@ namespace HybridImage
                     {
                         for (int templateX = 0; templateX < templateWidth; templateX++)
                         {
-                            pixel = bitmap1.GetPixel(imageX + templateX - xOffset, imageY + templateY - yOffset);
-                            r += (int)(pixel.R * convolutionTemplate[templateY, templateX]);
-                            g += (int)(pixel.G * convolutionTemplate[templateY, templateX]);
-                            b += (int)(pixel.B * convolutionTemplate[templateY, templateX]);
+                            // result  = source * kernel
+                            int x, y;
+                            x = imageX + templateX - xOffset;
+                            y = imageY + templateY - yOffset;
+                            r += source[y, x, 0] * kernel[templateY, templateX];
+                            g += source[y, x, 1] * kernel[templateY, templateX];
+                            b += source[y, x, 2] * kernel[templateY, templateX];
                         }
                     }
 
-                    pixel = System.Drawing.Color.FromArgb(r, g, b);
-                    i1.SetPixel(imageX, imageY, pixel);
+                    result[imageY, imageX, 0] = (byte)r;
+                    result[imageY, imageX, 1] = (byte)g;
+                    result[imageY, imageX, 2] = (byte)b;
                 }
             }
 
-            imageView3.Source = BitmapToBitmapSource(i1);
-
+            return result;
         }
 
-        private void TryOne()
+        private byte[, ,] Subtract(byte[, ,] data1, byte[, ,] data2)
         {
-            using (IplImage image = new IplImage(128, 128, BitDepth.U8, 1))
+            int height = data1.GetLength(0);
+            int width = data1.GetLength(1);
+            byte[, ,] result = new byte[height, width, 3];
+
+            for (int y = 0; y < height; y++)
             {
-                image.Zero();
-                for (int x = 0; x < image.Width; x++)
+                for (int x = 0; x < width; x++)
                 {
-                    for (int y = 0; y < image.Height; y++)
+                    result[y, x, 0] = (byte)(Math.Abs(data1[y, x, 0] - data2[y, x, 0]));
+                    result[y, x, 1] = (byte)(Math.Abs(data1[y, x, 1] - data2[y, x, 1]));
+                    result[y, x, 2] = (byte)(Math.Abs(data1[y, x, 2] - data2[y, x, 2]));
+                }
+            }
+
+            return result;
+        }
+
+        private byte[, ,] Brightness(byte[, ,] data, decimal factor)
+        {
+            int height = data.GetLength(0);
+            int width = data.GetLength(1);
+            int r, g, b;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    r = (int)(data[y, x, 0] * factor);
+                    g = (int)(data[y, x, 1] * factor);
+                    b = (int)(data[y, x, 2] * factor);
+
+                    if (r > 255)
                     {
-                        int offset = y * image.WidthStep + x;
-                        byte value = (byte)(x + y);
-                        Marshal.WriteByte(image.ImageData, offset, value);
+                        r = 255;
                     }
-                }
-                using (CvWindow window = new CvWindow(image))
-                {
-                    CvWindow.WaitKey();
+                    if (g > 255)
+                    {
+                        g = 255;
+                    }
+                    if (b > 255)
+                    {
+                        b = 255;
+                    }
 
+                    data[y, x, 0] = (byte)r;
+                    data[y, x, 1] = (byte)g;
+                    data[y, x, 2] = (byte)b;
                 }
             }
+
+            return data;
         }
 
-        private void TryTwo()
+        private byte[, ,] Increment(byte[, ,] data, int factor)
         {
-            using (var img = new IplImage(@"C:\Lenna.png"))
+            int height = data.GetLength(0);
+            int width = data.GetLength(1);
+            int r, g, b;
+
+            for (int y = 0; y < height; y++)
             {
-                Cv.SetImageROI(img, new CvRect(200, 200, 180, 200));
-                Cv.Not(img, img);
-                Cv.ResetImageROI(img);
-                using (new CvWindow(img))
+                for (int x = 0; x < width; x++)
                 {
-                    Cv.WaitKey();
+                    r = data[y, x, 0] + factor;
+                    g = data[y, x, 1] + factor;
+                    b = data[y, x, 2] + factor;
+
+                    if (r > 255)
+                    {
+                        r = 255;
+                    }
+                    if (g > 255)
+                    {
+                        g = 255;
+                    }
+                    if (b > 255)
+                    {
+                        b = 255;
+                    }
+
+                    data[y, x, 0] = (byte)r;
+                    data[y, x, 1] = (byte)g;
+                    data[y, x, 2] = (byte)b;
                 }
             }
+
+            return data;
+        }
+
+        private byte[, ,] Add(byte[, ,] data1, byte[, ,] data2)
+        {
+            int height = image1.GetLength(0);
+            int width = image1.GetLength(1);
+            byte[, ,] result = new byte[height, width, 3];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    result[y, x, 0] = (byte)((data1[y, x, 0] + data2[y, x, 0]) / 2);
+                    result[y, x, 1] = (byte)((data1[y, x, 1] + data2[y, x, 1]) / 2);
+                    result[y, x, 2] = (byte)((data1[y, x, 2] + data2[y, x, 2]) / 2);
+                }
+            }
+
+            return result;
+        }
+
+        private byte[, ,] BitmapToByteArray(System.Drawing.Bitmap bitmap)
+        {
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+            byte[, ,] data = new byte[height, width, 3];
+            System.Drawing.Color pixel;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    pixel = bitmap.GetPixel(x, y);
+                    data[y, x, 0] = pixel.R;
+                    data[y, x, 1] = pixel.G;
+                    data[y, x, 2] = pixel.B;
+                }
+            }
+
+            return data;
+        }
+
+        private System.Drawing.Bitmap ByteArrayToBitmap(byte[, ,] data)
+        {
+            int height = data.GetLength(0);
+            int width = data.GetLength(1);
+            System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    bitmap.SetPixel(x, y, System.Drawing.Color.FromArgb(data[y, x, 0], data[y, x, 1], data[y, x, 2]));
+                }
+            }
+
+            return bitmap;
+        }
+
+        private BitmapSource ByteArrayToBitmapSource(byte[, ,] data)
+        {
+            return BitmapToBitmapSource(ByteArrayToBitmap(data));
         }
 
         private BitmapSource BitmapToBitmapSource(System.Drawing.Bitmap source)
         {
-            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
-                          source.GetHbitmap(),
-                          IntPtr.Zero,
-                          Int32Rect.Empty,
-                          BitmapSizeOptions.FromEmptyOptions());
+            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(source.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
         }
 
         private System.Drawing.Bitmap BitmapSourceToBitmap(BitmapSource bitmapsource)
@@ -215,6 +434,46 @@ namespace HybridImage
                 bitmap = new System.Drawing.Bitmap(outStream);
             }
             return bitmap;
+        }
+
+        private void Finish(byte[, ,] i1, byte[, ,] i2, byte[, ,] h)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                buttonStart.Content = "Start";
+                buttonStart.IsEnabled = true;
+                imageView3.Source = ByteArrayToBitmapSource(i1);
+                imageView4.Source = ByteArrayToBitmapSource(i2);
+                imageView5.Source = ByteArrayToBitmapSource(h);
+                progressBar.Value = 0;
+                progressBar.Visibility = System.Windows.Visibility.Hidden;
+            });
+        }
+
+        private void UpdateProgressBar(double value)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                progressBar.Value = value;
+            });
+        }
+
+        private void DisposeObjects()
+        {
+            if (thread != null && thread.IsAlive)
+            {
+                thread.Abort();
+            }
+
+            if (image1 != null)
+            {
+                image1 = null;
+            }
+
+            if (image2 != null)
+            {
+                image2 = null;
+            }
         }
 
         #endregion
